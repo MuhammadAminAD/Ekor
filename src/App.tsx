6ebd9df822099777b7fa8f9e7a48974d61030188
Muhammad-amin7 <abduhalilovshohruh681@gmail.com>
2025-07-20 23:16:23 +0500

    redux firing and tuning

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-
+import AppRouter from "./router/index.route";
+import "@/i18n/i18next.config.ts"
 export default function App() {
     return (
-        <div>
-
-        </div>
+        <AppRouter />
     )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
+import { Provider } from 'react-redux'
+import { store } from './app/store'
 
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </BrowserRouter>
+  </Provider>
 )
