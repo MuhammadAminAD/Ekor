45558ee0be38ee31d438e5077bf9cb19413d98f3
izzat <izzatillabdurasulovv@gmail.com>
2025-07-26 20:49:58 +0500

    components folder added

---
+++ src/components/shared/Navbar.tsx
@@ -0,0 +1,14 @@
+import { styles } from "@/styles/index.styles"
+
+const Navbar = () => {
+  return (
+    <div className="w-full bg-[#F9F9F9] py-[25px]">
+     <div className={`${styles.Container}`}>
+
+     </div>
+
+    </div>
+  )
+}
+
+export default Navbar
--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,22 +1,25 @@
-import { Route, Routes } from 'react-router-dom'
-import Home from '../pages/Home'
+import { Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
 import { styles } from "../styles/index.styles";
+import Sidebar from "@/components/shared/Sidebar";
 
 export default function AppRouter() {
-      return (
-            <>
-                  <div className={styles.Container}>
-                        <div className="flex gap-1">
-                              <div className="max-w-[300px]">
-                                    {/* sidebar */}
-                              </div>
-                              <div className="w-full">
-                                    <Routes>
-                                          <Route element={<Home />} path="/" />
-                                    </Routes>
-                              </div>
-                        </div>
-                  </div>
-            </>
-      )
+  return (
+    <>
+    <div>
+      <div className={styles.Container}>
+        <div className="flex gap-1">
+          <div className="max-w-[240px] max-h-[940px] ">
+            <Sidebar />
+          </div>
+          <div className="w-full">
+            <Routes>
+              <Route element={<Home />} path="/" />
+            </Routes>
+          </div>
+        </div>
+      </div>
+      </div>
+    </>
+  );
 }
