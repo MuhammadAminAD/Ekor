0b51812092f47b4869412d7bcfc13f73402237cb
izzat <izzatillabdurasulovv@gmail.com>
2025-07-29 19:38:50 +0500

    products folder created

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,7 @@
+import Products from "@/components/products/Products";
+
 export default function Home() {
-  return <div className="pt-[100px] ">
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate fugit, illo voluptatibus sed unde mollitia odio ex sunt dignissimos tempora sit, corrupti consequuntur eveniet, dolorum nobis. Ut eligendi perferendis dicta illum. Est voluptatum iusto explicabo, reiciendis necessitatibus natus corrupti accusantium possimus dicta perferendis eum accusamus, animi quia aliquam illo dignissimos optio quae, voluptates autem tempora consectetur placeat. Deleniti animi nam saepe at odit vel obcaecati facilis similique tempora. Praesentium nesciunt vitae officiis magni vero autem, reprehenderit quasi, hic deserunt molestias perspiciatis laudantium inventore quis quo voluptatem obcaecati. Ratione autem quasi ullam consequuntur eos deserunt porro accusantium optio modi, ex earum!</p>
+  return <div>
+    <Products/>
   </div>;
 }
