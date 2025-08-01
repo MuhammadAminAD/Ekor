ca993d8d1e012e6f6b5873167b1d944d93d531f6
izzat <izzatillabdurasulovv@gmail.com>
2025-08-01 19:54:06 +0500

    user page qo'shildi

--- src/features/TokenSlice.ts
+++ src/features/TokenSlice.ts
@@ -1,31 +1,31 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
 interface AuthState {
-      token: {
-            accessToken: string | null
-      }
+  token: {
+    accessToken: string | null;
+  };
 }
 
 const initialState: AuthState = {
-      token: {
-            accessToken: localStorage.getItem('token'),
-      }
-}
+  token: {
+    accessToken: localStorage.getItem("token"),
+  },
+};
 
 const TokenSlice = createSlice({
-      name: 'auth',
-      initialState,
-      reducers: {
-            setToken: (state, action) => {
-                  state.token = action.payload
-                  localStorage.setItem('token', action.payload)
-            },
-            removeToken: (state) => {
-                  state.token = { accessToken: null }
-                  localStorage.removeItem('token')
-            },
-      },
-})
+  name: "auth",
+  initialState,
+  reducers: {
+    setToken: (state, action) => {
+      state.token = action.payload;
+      localStorage.setItem("token", action.payload);
+    },
+    removeToken: (state) => {
+      state.token = { accessToken: null };
+      localStorage.removeItem("token");
+    },
+  },
+});
 
-export const { setToken, removeToken } = TokenSlice.actions
-export default TokenSlice.reducer
+export const { setToken, removeToken } = TokenSlice.actions;
+export default TokenSlice.reducer;
--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Authentication from "@/pages/Authentication";
+import User from "@/pages/User";
 
 export default function AppRouter() {
   return (
@@ -8,6 +9,7 @@ export default function AppRouter() {
       <Routes>
         <Route element={<Authentication />} path="/auth" />
         <Route element={<Home />} path="/" />
+        <Route element={<User />} path="/user" />
       </Routes>
     </>
   );
