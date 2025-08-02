734741ac0706d7759e1f4081d1ad4c69debc1700
izzat <izzatillabdurasulovv@gmail.com>
2025-08-02 17:41:46 +0500

    %

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Authentication from "@/pages/Authentication";
 import User from "@/pages/User";
+import CoursePage from "@/pages/CoursePage";
 
 export default function AppRouter() {
   return (
@@ -9,6 +10,7 @@ export default function AppRouter() {
       <Routes>
         <Route element={<Authentication />} path="/auth" />
         <Route element={<Home />} path="/" />
+        <Route element={<CoursePage />} path="/course" />
         <Route element={<User />} path="/user" />
       </Routes>
     </>
